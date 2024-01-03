6b8a1bb9661bf153497c9d3915c7d7fc07555f0e
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-01-03 21:50:04 +0530

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://inatorsui.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://inatorsui.vercel.app/docs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://inatorsui.vercel.app/inators",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
