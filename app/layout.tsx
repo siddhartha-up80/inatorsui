66e6734515fe8e90f0c88107ae999c487e0f9b63
Siddhartha Singh <siddhartha.singh@tecnod8.com>
2024-06-02 15:52:02 +0530

    added head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import MainNav from "@/components/mainNav";
 import MainFooter from "@/components/mainFooter";
 import { Toaster } from "sonner";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,13 @@ export default function RootLayout({
         content="wuKkVFD1dhs31EjQNk81yBbw-temshhMPPc8JGAUC94"
       />
       <meta name="google-adsense-account" content="ca-pub-9924264215171914" />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9924264215171914"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className={inter.className}>
         <div className="">
           <MainNav />
